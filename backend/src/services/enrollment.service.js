5dc19f0373c986d6a224203c205a9ea800245e5f
Neo Pham <hieupham250201@gmail.com>
2025-12-16 14:49:55 +0700

    feat(backend): include course ratingAvg in enrollment responses

--- backend/src/services/enrollment.service.js
+++ backend/src/services/enrollment.service.js
@@ -101,6 +101,7 @@ class EnrollmentService {
                                     avatarUrl: true,
                                 },
                             },
+                            ratingAvg: true,
                         },
                     },
                 },
