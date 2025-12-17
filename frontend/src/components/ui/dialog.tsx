4b4f481878d4be2db02e73f7fe134b1c27ad10c2
itzdazk <namle27102003@gmail.com>
2025-12-17 17:32:56 +0700

    feat: add wide prop to dialog component

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -51,9 +51,11 @@ function DialogContent({
   className,
   children,
   container,
+  wide = false,
   ...props
 }: React.ComponentProps<typeof DialogPrimitive.Content> & {
   container?: HTMLElement | null;
+  wide?: boolean;
 }) {
   return (
     <DialogPortal data-slot="dialog-portal" container={container}>
@@ -61,7 +63,8 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[100] grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
+          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[100] grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200",
+          wide ? "sm:max-w-[1600px] w-[95vw]" : "sm:max-w-lg",
           className,
         )}
         {...props}
