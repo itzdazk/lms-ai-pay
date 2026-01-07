d51f8b18defb17ae9302fd5ec98f242ec019db8e
itzdazk <namle27102003@gmail.com>
2026-01-07 09:57:02 +0700

    refactor: remove bookmarks and learning_goals relations

---
+++ backend/prisma/migrations/20260107024158_remove_bookmarks_learning_goals/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "public"."idx_enrollments_user_status";
+
+-- DropIndex
+DROP INDEX "public"."idx_progress_user_completed";
+
+-- DropIndex
+DROP INDEX "public"."idx_quiz_submissions_user_date";
